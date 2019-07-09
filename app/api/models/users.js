0e158a0617f8971ff5e414af2a35489e7a9fa60a
Dimas Febri <dfebri446@gmail.com>
2019-07-09 08:35:48 +0700

    add user model and controller

---
+++ app/api/models/users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+//Define a schema
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+	
+	
+})
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express 	 = require('express')
 const logger	 = require('morgan')
 const bodyParser = require('body-parser')
 
-
 const app	 = express()
 
 app.use(logger('dev'))
