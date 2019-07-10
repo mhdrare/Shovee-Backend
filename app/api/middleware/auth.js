985b85e341efa97ace716d039f8c03e3e9b701f4
Dimas Febri <dfebri446@gmail.com>
2019-07-10 09:56:11 +0700

    add middleware auth

---
+++ app/api/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+ 
+module.exports = function (req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).json({
+            status: 'failed',
+            message: 'Access denied. No JWT provided.'
+        });
+    }
+ 
+    try {
+        const decoded = jwt.verify(token, config.get('PrivateKey'));
+        req.user = decoded;
+        next();
+    }
+    catch (ex) {
+        res.status(400).json({
+            status: 'failed',
+            message: 'Invalid JWT.'
+        });
+    }
+}
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,10 +4,13 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../app/api/controllers/products.controller');
 
+// middleware
+const auth = require('../app/api/middleware/auth')
+
 router.get('/', productsController.findAll)
 router.get('/:id', productsController.findById)
-router.post('/', productsController.create)
-router.patch('/:id', productsController.update)
-router.delete('/:id', productsController.delete)
+router.post('/', auth, productsController.create)
+router.patch('/:id', auth, productsController.update)
+router.delete('/:id', auth, productsController.delete)
 
 module.exports = router
