f05d0d379155dc2d2247c554aa6090cde92d7857
Dimas Febri <dfebri446@gmail.com>
2019-07-11 14:15:00 +0700

    add chngae password

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,8 +4,12 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../app/api/controllers/users.controllers')
 
-router.post('/register', userController.create);
-router.post('/authenticate', userController.auth);
-router.post('/forgetpassword', userController.forgetPassword);
+// middleware
+const auth = require('../app/api/middleware/auth')
+
+router.post('/register', userController.create)
+router.post('/authenticate', userController.auth)
+router.post('/forgetpassword', userController.forgetPassword)
+router.patch('/changepassword', auth, userController.changePassword)
 
 module.exports = router
